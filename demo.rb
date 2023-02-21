1f43ace88ba6b52e99e1404eb30e9c6564fefda0
Rodrigo Von Bischoffshausen <rodrigo.von.ing@gmail.com>
2023-02-21 15:36:47 -0300

    Primer Ejercicio demo 1200

---
+++ demo.rb
@@ -0,0 +1,8 @@
+def circle_area(radius)
+   # TODO: Implement the `circle_area` method
+   return 0 if radius < 0
+   return 3.14 if radius == 1
+   return 3.14 * (radius * radius)
+
+end
+puts circle_area (4)
